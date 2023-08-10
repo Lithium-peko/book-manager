96da6e2032ac043f9c619b85d927241296ac05d1
peko <lithiumreona@gmail.com>
2023-08-10 23:16:48 +0800

    config commit

--- book-service/src/main/java/com/example/Controller/BookController.java
+++ book-service/src/main/java/com/example/Controller/BookController.java
@@ -3,6 +3,7 @@ package com.example.Controller;
 import com.example.Service.BookService;
 import com.example.entity.Book;
 import jakarta.annotation.Resource;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +14,8 @@ public class BookController {
     BookService service;
 
     @RequestMapping("book/{bid}")
-    public Book findBookById(@PathVariable("bid") int bid) {
+    public Book findBookById(@PathVariable("bid") int bid, HttpServletRequest request) {
+        System.out.println(request.getHeader("Test"));
         return service.getBookById(bid);
     }
 }
